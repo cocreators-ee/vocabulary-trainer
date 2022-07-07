875325ba60355f9bb0b46ade42e931d2cdfc5e2d
Henry <henry.elmest@cocreators.ee>
2022-07-07 18:11:41 +0300

    setup store

--- frontend/src/lib/Main.svelte
+++ frontend/src/lib/Main.svelte
@@ -1,22 +1,78 @@
 <script>
-  import Button from './Button.svelte'
+  import { Button } from 'carbon-components-svelte'
   import { SkeletonPlaceholder } from 'carbon-components-svelte'
+  import { currentWord, skipNext } from './stores'
+  import { onDestroy } from 'svelte'
 
   import 'carbon-components-svelte/css/g10.css'
   import 'carbon-components-svelte/css/all.css'
+
+  let skeleton = { show: false }
+
+  function toggleTranslation() {
+    skeleton.show = !skeleton.show
+  }
+
+  function nextWords() {
+    skipNext()
+    skeleton.show = !skeleton.show
+  }
+
+  function handleKeydown(event) {
+    if (skeleton.show) {
+      nextWords()
+    } else {
+      if (event.key === 'Enter' || event.code === 'Space') {
+        skeleton.show = !skeleton.show
+      }
+    }
+  }
+
+  let word
+  let translation
+
+  const unsubscribe = currentWord.subscribe((value) => {
+    word = value.source
+    translation = value.translations[0].word
+  })
+
+  onDestroy(unsubscribe)
 </script>
 
+<svelte:window on:keydown={handleKeydown} />
 <main>
-  <h2>Tere</h2>
+  <h2 class="heading">{word}</h2>
   <div class="placeholder">
-    <SkeletonPlaceholder style="height: 8rem;  width: 14rem;" />
+    {#if skeleton.show}
+      <p>{translation}</p>
+    {:else}
+      <button on:click={toggleTranslation}>
+        <SkeletonPlaceholder style="height: 8rem;  width: 14rem;" />
+      </button>
+    {/if}
   </div>
 
-  <Button />
+  {#if skeleton.show}
+    <Button kind="primary" class="translate-button" on:click={nextWords}
+      >Next word
+    </Button>
+  {:else}
+    <Button kind="primary" class="translate-button" on:click={toggleTranslation}
+      >Show translation
+    </Button>
+  {/if}
 </main>
 
 <style>
-  h2 {
+  :global(.translate-button) {
+    padding: 1rem 2.5rem;
+  }
+
+  button {
+    border: none;
+  }
+
+  .heading {
     font-weight: 700;
     font-size: 3rem;
   }
---
+++ frontend/src/lib/stores.js
@@ -0,0 +1,16 @@
+import words from '../languages/et/words.json'
+import { writable } from 'svelte/store'
+
+function randInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+function getRandomWord() {
+  return words[randInt(0, words.length - 1)]
+}
+
+export const currentWord = writable(getRandomWord())
+
+export function skipNext() {
+  currentWord.set(getRandomWord())
+}
