4a8abc73c40fa10e01d22f070901f7195d0ac377
Henry <henry.elmest@cocreators.ee>
2022-07-12 17:27:50 +0300

    LanguageSelector

---
+++ frontend/src/lib/LanguageSelector.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Link } from 'carbon-components-svelte'
+  import 'carbon-components-svelte/css/g10.css'
+  import 'carbon-components-svelte/css/all.css'
+</script>
+
+<nav><Link class="lang">Estonian</Link></nav>
+
+<style>
+  nav {
+    display: flex;
+    margin-left: auto;
+  }
+  :global(.lang) {
+    color: #000;
+    text-decoration: underline;
+  }
+</style>
