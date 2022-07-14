a5279485cdf298a559b9946b6615b488111987a9
Henry <henry.elmest@cocreators.ee>
2022-07-14 11:40:07 +0300

    fixes for Reveal translation

--- frontend/src/lib/LanguageSelector.svelte
+++ frontend/src/lib/LanguageSelector.svelte
@@ -1,10 +1,12 @@
 <script>
-  import { Link } from 'carbon-components-svelte'
+  import { Link, Modal } from 'carbon-components-svelte'
   import 'carbon-components-svelte/css/g10.css'
   import 'carbon-components-svelte/css/all.css'
 </script>
 
 <nav><Link class="lang">Estonian</Link></nav>
+
+<Modal open={true}>lalalal</Modal>
 
 <style>
   nav {
