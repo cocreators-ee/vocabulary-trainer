d71be06537a3ff554d37d6d1c8aca56ccebce25b
Henry <henry.elmest@cocreators.ee>
2022-07-14 13:19:07 +0300

    update store

--- frontend/src/lib/stores.js
+++ frontend/src/lib/stores.js
@@ -1,12 +1,21 @@
-import words from '../languages/et/words.json'
+import et from '../languages/et/words.json'
+import fi from '../languages/fi/words.json'
 import { writable } from 'svelte/store'
+import languages from '../languages/languages.json'
+
+const words = {
+  et,
+  fi,
+}
+
+const currentLanguage = languages[0]
 
 function randInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min
 }
 
 function getRandomWord() {
-  return words[randInt(0, words.length - 1)]
+  return words[currentLanguage.code][randInt(0, words[currentLanguage.code].length - 1)]
 }
 
 export const currentWord = writable(getRandomWord())
